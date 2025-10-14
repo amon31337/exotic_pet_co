c81d7729e9f63adaa9de8db3819dc1abade9b4ae
amon31337 <amon.18@buckeyemail.osu.edu>
2025-10-13 20:29:37 -0400

    reorganized code (#11)
    * reorganized code
    
    * recreate public folder

--- src/components/viewOrder.js
+++ src/components/viewOrder.js
@@ -1,8 +1,8 @@
 // src/components/viewOrder.js
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useCart } from "../context/CartContext";
-import { PaymentContext } from "../context/PaymentContext";
+import { useCart } from "../context/CartContext.js";
+import { PaymentContext } from "../context/PaymentContext.js";
 import { computeTotals } from "../helpers/priceHelper.js";
 
 const ViewOrder = () => {
