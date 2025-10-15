aaa6200ef69c04298f3211d7444116aac7109d7f
Benton Amon <amon.18@buckeyemail.osu.edu>
2025-10-15 15:10:13 -0400

    create home and about page

---
+++ src/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8 text-center">
+          <h1 className="display-4 mb-4">About Exotic Pet Co.</h1>
+          <p className="lead">
+            Learn more about our mission to provide extraordinary exotic pets.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
