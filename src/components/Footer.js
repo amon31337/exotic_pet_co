16e658ca4085c17b3975480aecf7b190161cde47
Benton Amon <amon.18@buckeyemail.osu.edu>
2025-10-21 13:42:03 -0400

    Fixed typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-light py-3 mt-auto text-center">
       <div className="container">
-        <p className="mb-1">© 2025 ExoticPetOo, Inc. All rights reserved.</p>
+        <p className="mb-1">© 2025 ExoticPetCo, Inc. All rights reserved.</p>
         <p className="small mb-0">
           <a href="/about" className="text-decoration-none text-light me-3">About Us</a>
           <a href="/contactUs" className="text-decoration-none text-light me-3">Contact Us</a>
