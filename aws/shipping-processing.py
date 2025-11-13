91b9a6d1d17177d6363946a4458a1e525f49d959
jackson-boes-1 <boes.79@osu.edu>
2025-11-13 14:36:09 -0500

    Update shipping-processing.py
    add asynchronous message to send shipping token to order management database

--- aws/shipping-processing.py
+++ aws/shipping-processing.py
@@ -3,6 +3,8 @@ import logging
 import pymysql
 import json
 import os
+import boto3
+import random
 
 user_name = os.environ['USER_NAME']
 password = os.environ['PASSWORD']
@@ -29,13 +31,33 @@ def lambda_handler(event, context):
     shippingWeights = detail['shippingWeights']
 
     with conn.cursor() as cursor:
-        cursor.execute(f"insert into SHIPPING_INFO (ADDRESS1, ADDRESS2, CITY, STATE, POSTAL_CODE) VALUES ('{shippingInfo['addressLine1']}', '{shippingInfo['addressLine2']}', '{shippingInfo['city']}', '{shippingInfo['state']}', '{shippingInfo['zip']}')")
+        #create shipping confirmation token
+        token = random.randint(100000, 999999)
+
+        cursor.execute(f"insert into SHIPPING_INFO (CONFIRMATION_TOKEN, ADDRESS1, ADDRESS2, CITY, STATE, POSTAL_CODE) VALUES ('{token}', '{shippingInfo['addressLine1']}', '{shippingInfo['addressLine2']}', '{shippingInfo['city']}', '{shippingInfo['state']}', '{shippingInfo['zip']}')")
         logging.info("Shipping info inserted")
 
         for weight in shippingWeights:
-            cursor.execute(f"insert into PACKET (ITEM_ID, SHIPPING_INFO_ID, WEIGHT) VALUES ('{weight['id']}', (SELECT ID FROM SHIPPING_INFO WHERE ADDRESS1 = '{shippingInfo['addressLine1']}' AND ADDRESS2 = '{shippingInfo['addressLine2']}' AND CITY = '{shippingInfo['city']}' AND STATE = '{shippingInfo['state']}' AND POSTAL_CODE = '{shippingInfo['zip']}' LIMIT 1), '{weight['weight']}')")
+            cursor.execute(f"insert into PACKET (ITEM_ID, SHIPPING_INFO_ID, WEIGHT) VALUES ('{weight['id']}', (SELECT ID FROM SHIPPING_INFO WHERE CONFIRMATION_TOKEN = '{token}' AND ADDRESS1 = '{shippingInfo['addressLine1']}' AND ADDRESS2 = '{shippingInfo['addressLine2']}' AND CITY = '{shippingInfo['city']}' AND STATE = '{shippingInfo['state']}' AND POSTAL_CODE = '{shippingInfo['zip']}' LIMIT 1), '{weight['weight']}')")
 
         conn.commit()
+
+        #send shipping token back to caller
+        eventbridge = boto3.client('events')
+        detail = {
+            'businessID': detail['businessID'], 
+            'orderID': detail['orderID'], 
+            'confirmationToken': token
+        }
+        eventbridge.put_events(
+            Entries=[
+                {
+                    'Source': 'shippingProcessing',
+                    'DetailType': 'shipping-token',
+                    'Detail': json.dumps(detail)
+                }
+            ]
+        )
         
     
     return {
